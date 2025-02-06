7197d748f6fccffdcae6cc8d65f8732977459d89
PJ <joppy007@gmail.com>
2025-02-06 17:26:12 -0500

    changes

--- index.js
+++ index.js
@@ -45,7 +45,10 @@ app.post('/api/courses', (req, res) => {
 //Update
 app.put('/api/courses', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) res.status(404).send('Course not found');
+    if (!course) return res.status(404).send('Course not found');
+
+    const {error} = validateCourse(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
 
 const schema = {
@@ -67,20 +70,25 @@ function validateCourse(course){
     }
 }
 
+
+
 //Delete
 app.delete('/api/courses', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) res.status(404).send('Course not found');
+
+    //Params for Delete and return error if course not found
+    const course = courses.find(c => c.id === parseInt(req.params.id)) //access the id parameter
+    if (!course) return res.status(404).send('Course not found');
 
     const index = courses.indexOf(course);
     course.splice(index, 1);
+    
     res.send(course);
 });
 
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) res.status(404).send('Course not found');
+    if (!course) return res.status(404).send('Course not found');
         res.send(course);
 });
 
